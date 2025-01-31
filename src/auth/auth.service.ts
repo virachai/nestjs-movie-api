7a062988a8e43aaa3e49cbccf82ba7b482c40ed4
Virachai Wongsena <wvirachai@gmail.com>
2025-01-31 20:05:20 +0700

    update Authentication

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signIn(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+    const userWithoutPassword = { ...user };
+    delete userWithoutPassword.password;
+
+    // TODO: Generate a JWT and return it here
+    // instead of the user object
+    return userWithoutPassword;
+  }
+}
---
+++ src/auth/dto/sign-in.dto.ts
@@ -0,0 +1,18 @@
+// sign-in.dto.ts
+export class SignInDto {
+  username: string;
+  password: string;
+}
+
+// // sign-in.dto.ts
+// import { IsString, IsNotEmpty } from 'class-validator';
+
+// export class SignInDto {
+//   @IsString()
+//   @IsNotEmpty()
+//   username: string;
+
+//   @IsString()
+//   @IsNotEmpty()
+//   password: string;
+// }
