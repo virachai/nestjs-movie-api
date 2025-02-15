ec656f73ab48f7e31f86900a45f4da049d001dc9
Virachai Wongsena <wvirachai@gmail.com>
2025-02-15 14:36:25 +0700

    Explicitly Allow Specific Origins

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS for all routes
+  // app.enableCors({
+  //   origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   allowedHeaders: 'Content-Type, Accept',
+  // });
   app.enableCors({
-    origin: '*',
+    origin: ['https://nextjs-mee-webboard.vercel.app', 'http://localhost:3000'], // Add both URLs
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
   });
